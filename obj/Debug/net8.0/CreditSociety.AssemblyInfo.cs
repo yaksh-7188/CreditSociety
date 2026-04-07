cadcdd809c74808a03a796bf7bfc76162959a7ea
Yaksh patel <patelyaksh281256@gmail.com>
2026-04-07 08:07:34 +0530

    final

--- obj/Debug/net8.0/CreditSociety.AssemblyInfo.cs
+++ obj/Debug/net8.0/CreditSociety.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreditSociety")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0259923d285801fc3f450af9f5377d4eaa985671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0db811d530d91d9c951509e596d2c6e2c8062d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreditSociety")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreditSociety")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
